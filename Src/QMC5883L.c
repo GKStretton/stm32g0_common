3e74f2a63500419cf55863172e1f924e2855a9be
Greg Stretton <greg.stretton@protonmail.com>
2021-03-31 16:25:01 +0100

    Fix hard-fault bug

--- Src/QMC5883L.c
+++ Src/QMC5883L.c
@@ -11,12 +11,23 @@ void QMC5883L_Init(UART_HandleTypeDef *uart, I2C_HandleTypeDef *i2c, uint8_t rat
 {
 	// 0000RR01 10:00 50:01 100:10 200:11
 	writeOneByte(i2c, QMC_ADDRESS, CR1_REGISTER, 0b00000001 | (rateSetting << 2));
+
+	  /*
+	  35168 Bytes GOOD
+	  35176 Bytes FAULT (2ledOns in main)
+	  35420 Bytes FAULT (43 ledOns in main)
+	  35428 Bytes GOOD
+	  https://electronics.stackexchange.com/questions/498286/how-to-debug-this-i2c-hard-fault
+	  https://interrupt.memfault.com/blog/cortex-m-fault-debug#registers-prior-to-exception
+	  Check on other hardware
+	  */
+	/* Cause of the strangest bug ever. Program halting through this, but only when the program was a certain size.
 	if (getWhoIs() != 0xFF)
 	{
 		print(uart, "QMC5883L Incorrect ID:");
 		uint8_t val = getWhoIs();
 		HAL_UART_Transmit(uart, (uint8_t*) &val, 1, 1000);
-	}
+	}*/
 
 	print(uart, "QMC5883L initialisation complete.\n");
 }
